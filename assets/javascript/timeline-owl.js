2c1b3a67d0af9672d6b86d51736a63226b84747a
Joseph Anderson <joseph_anderson@fitnyc.edu>
2019-07-18 11:10:37 -0400

    update gem file

--- assets/javascript/timeline-owl.js
+++ assets/javascript/timeline-owl.js
@@ -174,15 +174,3 @@ $(document).ready(function(){
       owl.trigger('prev.owl.carousel');
   });
 });
-//Only load the tag carousel after the entire page loads because font rendering messes up the auto width
-$(window).bind("load", function() {
-  var tagOwl = $('#tag-carousel');
-  tagOwl.owlCarousel({
-    autoWidth:true,
-    loop: true,
-    margin: 30,
-    items: 27,
-    nav: false,
-    dots: false
-  });
-});
